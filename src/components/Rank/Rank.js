84e8b2d5393810ebcb5969f88b33ad639446df41
MrMischievousX <2020kucp1138@iiitkota.ac.in>
2021-04-28 14:42:53 +0530

    make it better

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, You can enter any Url`}
+      </div>
+      <div className='white f1'>
+        { }
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
